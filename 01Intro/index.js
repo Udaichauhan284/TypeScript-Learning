75b131d3b19a9c3a2b4a7238bcd6d9cdd3004516
Udai Chauhan <udaichauhan284@gmail.com>
2024-01-02 19:19:01 +0530

    Adding 01intro and notes- variables, Any

---
+++ 01Intro/index.js
@@ -0,0 +1,18 @@
+//variables
+//Primitive Data types: Number, String and boolean
+//String
+var greeting = "hello Udai";
+console.log(greeting);
+//Number - in JS there is no Float and int - we have simple Number which covers all int and float
+var userId = 34.5;
+console.log(userId);
+//Boolean
+var isLogIn = false;
+isLogIn.valueOf();
+console.log(isLogIn);
+//any
+var hero;
+function myHero() {
+    return "Iron Man";
+}
+hero = myHero();
---
+++ 01Intro/index.ts
@@ -0,0 +1,28 @@
+//variables
+
+//Primitive Data types: Number, String and boolean
+
+//String
+let greeting: string = "hello Udai";
+
+console.log(greeting);
+
+//Number - in JS there is no Float and int - we have simple Number which covers all int and float
+
+let userId: number= 34.5;
+
+console.log(userId);
+
+//Boolean
+let isLogIn: boolean = false;
+isLogIn.valueOf();
+console.log(isLogIn);
+
+//any
+let hero: any;
+function myHero(){
+  return "Iron Man";
+}
+hero = myHero();
+
+
